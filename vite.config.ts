076f7b05072297dda3ae7d39fe5f3454949ea29a
Hossam Hamza <hossamhamza.hm@gmail.com>
2023-11-21 10:24:22 +0200

    fix/ fix vite build config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/university-schedule-generator-reac/',
+  base: './',
   plugins: [react()],
 })
