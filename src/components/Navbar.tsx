495755c95c8d58dc95167717ab115f07856d0319
Hossam Hamza <hossamhamza.hm@gmail.com>
2023-11-22 14:39:20 +0200

    fix/ gh spa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,14 +56,14 @@ export default function Navbar(){
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav w-100">
                         <li className="nav-item">
-                        <NavLink className="nav-link" aria-current="page" to="/home">Home</NavLink>
+                        <NavLink className="nav-link" aria-current="page" to="./home">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                        <NavLink className="nav-link" to="courses">Courses</NavLink>
+                        <NavLink className="nav-link" to="./courses">Courses</NavLink>
                         </li>
 
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="schedules">Schedules</NavLink>
+                            <NavLink className="nav-link" to="./schedules">Schedules</NavLink>
                         </li>
 
                         <li className="nav-item ms-auto">
