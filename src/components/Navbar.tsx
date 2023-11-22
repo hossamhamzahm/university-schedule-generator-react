0820e1c23df2bbefd58c258e899a8141d73e3121
Hossam Hamza <hossamhamza.hm@gmail.com>
2023-11-22 13:17:35 +0200

    feat/ visit count feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+
+
+const Config = {
+    baseURL: 'https://gadwelooh-api.ddns.net',
+    headers: {
+        // 'Accept': 'application/vnd.GitHub.v3+json',
+        //'Authorization': 'token <your-token-here> -- https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
+    },
+    transformResponse: [
+        (data: string) => JSON.parse(data),
+    ],
+}
+
+
+async function get_cnt(): Promise<{"id": number, "total_cnt": number, "successfull_cnt": number}> {
+    const Client = axios.create(Config)
+    const cnt = await Client.get<{
+        "id": number, "total_cnt": number, "successfull_cnt": number }>("/visits");
+
+    return cnt.data;
+}
+
+
+
 export default function Navbar(){
+    const [cnt, set_cnt] = useState({ "id": -1, "total_cnt": -1, "successfull_cnt": -1 });
+
+    const update_cnt = async () => {
+        const cnt = await get_cnt();
+        set_cnt(cnt);
+    }
+
+    useEffect(() => {
+        update_cnt();
+    }, [cnt])
+    
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
@@ -11,7 +48,7 @@ export default function Navbar(){
 
 
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav w-100">
                         <li className="nav-item">
                         <NavLink className="nav-link" aria-current="page" to="/home">Home</NavLink>
                         </li>
@@ -21,6 +58,10 @@ export default function Navbar(){
 
                         <li className="nav-item">
                             <NavLink className="nav-link" to="schedules">Schedules</NavLink>
+                        </li>
+
+                        <li className="nav-item ms-auto">
+                            <div className="nav-link" ><code>#</code> Of App Usages: <code>{cnt.total_cnt}</code></div>
                         </li>
                         {/* <li className="nav-item">
                         <NavLink className="nav-link" to="#">Pricing</NavLink>
